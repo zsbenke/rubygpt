dd86ef49b03825af564464c724bf5d152f12cd97
Zsolt Benke <zsolt@decoding.io>
2023-03-05 16:55:12 +0100

    Add an argument to convert between session formats

--- lib/rubygpt.rb
+++ lib/rubygpt.rb
@@ -16,7 +16,7 @@ module Rubygpt
     OptionParser.new do |opts|
       opts.banner = "Usage: rubygpt [options]"
 
-      opts.on("-s", "--session-path PATH", "Path to a session file") do |path|
+      opts.on("-s", "--session-path PATH", "Load an existing session file") do |path|
         options[:session_path] = File.expand_path(path.chomp)
       end
 
@@ -24,15 +24,14 @@ module Rubygpt
         options[:convert] = File.expand_path(path.chomp)
       end
 
-      opts.on("-p", "--process PATH", "Path to a file to process") do |path|
+      opts.on("-p", "--process PATH", "Silently process a session file and then save") do |path|
         options[:process_path] = File.expand_path(path.chomp)
       end
 
-      opts.on("-f", "--format FORMAT", "Format of the context file") do |format|
+      opts.on("-f", "--format FORMAT", "Set the format of the session file") do |format|
         options[:format] = format.chomp
       end
     end.parse!
-
 
     session_path = options[:session_path] if options[:session_path]
     process_path = options[:process_path] if options[:process_path]
@@ -47,7 +46,7 @@ module Rubygpt
     end
 
     if convert
-      chat = Rubygpt::Chat.new(ENV["OPENAI_API_KEY"], session_path: session_path, format: format, output: false)
+      chat = Rubygpt::Chat.new(ENV["OPENAI_API_KEY"], session_path: convert, format: format, output: false)
       chat.convert
 
       return
